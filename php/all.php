32746a37dd4ec4b5fabd0130c47711bd004cc781
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2021-02-26 02:39:12 +0300

    Update all.php

--- php/all.php
+++ php/all.php
@@ -5,11 +5,11 @@ $status = $_POST['status'];
 
 if ( $status == "" )
 {
-	$result = mysqli_query($link, "select * from adminsKATE");
+	$result = mysqli_query($link, "select * from AdminsFeedback");
 }
 else
 {
-	$result = mysqli_query($link, "select * from adminsKATE where status='{$status}'");
+	$result = mysqli_query($link, "select * from AdminsFeedback where status='{$status}'");
 }
 
 $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
