1f6cb797f5578ed836f1b40b9e0b57b382eab223
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2021-03-04 13:42:11 +0300

    Update AdminsFeedback.sql

--- AdminsFeedback.sql
+++ AdminsFeedback.sql
@@ -1,3 +1,4 @@
+-- СОЗДАНИЕ ТАБЛИЦЫ С ПОЛЬЗОВАТЕЛЯМИ И АДМИНАМИ:
 DROP TABLE IF EXISTS `AdminsFeedback`;
 CREATE TABLE `AdminsFeedback` (
   `id` int(11) NOT NULL,
@@ -6,14 +7,30 @@ CREATE TABLE `AdminsFeedback` (
   `username` varchar(64) CHARACTER SET utf8 NOT NULL,
   `status` varchar(64) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
 INSERT INTO `AdminsFeedback` (`id`, `tgid`, `name`, `username`, `status`) VALUES
 (1, 'ТВОЙ АЙДИ В ТЕЛЕГЕ', 'ТВОЁ ИМЯ', 'ТВОЙ ЮЗЕРНЕЙМ', 'god');
-
 -- ВАЖНО! --> ПОСЛЕ ЮЗЕРНЕЙМА НЕ МЕНЯТЬ "god" НА ДРУГОЕ, ЭТО ЗНАЧИТ СТАТУС БОГА, А-ЛЯ САМЫЙ ГЛАВНЫЙ АДМИН !!!
-
 ALTER TABLE `AdminsFeedback`
   ADD PRIMARY KEY (`id`);
-
 ALTER TABLE `AdminsFeedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+
+-- СОЗДАНИЕ ТАБЛИЦЫ С СООБЩЕНИЯМИ
+DROP TABLE IF EXISTS `MessFeedback`;
+CREATE TABLE `MessFeedback` (
+  `id` int(11) NOT NULL,
+  `tgid` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `username` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `text` text CHARACTER SET utf8 NOT NULL,
+  `answer` text CHARACTER SET utf8 NOT NULL,
+  `status` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `adminID` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `adminName` varchar(64) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+ALTER TABLE `MessFeedback`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `MessFeedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
