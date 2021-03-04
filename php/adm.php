697fb62e0f001de0c9cf22dd266d48f450dbb48c
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2021-03-04 13:36:36 +0300

    Update adm.php

--- php/adm.php
+++ php/adm.php
@@ -1,15 +1,13 @@
 <?php
 require "bd.php";
-
 $data = $_POST['data'];
-
 $tgid = $_POST['tgid'];
 $username = $_POST['username'];
 $name = $_POST['name'];
 
 if ($data == "addadmin")
 {
-	$result = mysqli_query($link, "INSERT INTO `AdminsFeedback` (`tgid`, `name`, `username`, `status`) VALUES ('{$tgid}', '', '', 'admin')");
+	$result = mysqli_query($link, "INSERT INTO `AdminsFeedback` (`tgid`, `status`) VALUES ('{$tgid}', 'admin')");
 }
 elseif ($data == "update")
 {
@@ -19,4 +17,9 @@ elseif ($data == "delete")
 {
 	$result = mysqli_query($link, "DELETE FROM `AdminsFeedback` WHERE `tgid`='{$tgid}'");
 }
+elseif ($data == "adduser")
+{
+	$result = mysqli_query($link, "INSERT INTO `AdminsFeedback` (`tgid`, `name`, `username`, `status`) VALUES ('{$tgid}','{$name}','{$username}', 'user') ");
+}
+
 ?>
