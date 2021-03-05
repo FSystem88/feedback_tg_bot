4364f588eb4def2060baff97c2f130cf98e04b2c
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2021-03-05 03:46:57 +0300

    Update all.php

--- php/all.php
+++ php/all.php
@@ -3,20 +3,13 @@ require "bd.php";
 
 $status = $_POST['status'];
 
-if ( $status == "admins" )
-{
+if ($status == "admins") {
 	$result = mysqli_query($link, "SELECT * FROM AdminsFeedback WHERE status NOT LIKE 'user'");
-}
-elseif ( $status == "god" )
-{
+} elseif ($status == "god") {
 	$result = mysqli_query($link, "SELECT * FROM AdminsFeedback WHERE status='god'");
-}
-elseif ( $status == "user" )
-{
+} elseif ($status == "user") {
 	$result = mysqli_query($link, "SELECT * FROM AdminsFeedback WHERE status='user'");
 }
 
 $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
 echo json_encode($row);
-
-?>
