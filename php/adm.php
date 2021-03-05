21fdd35aaa6fdd33c5b9aec678158e1ed00df4c5
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2021-03-05 03:46:23 +0300

    Update adm.php

--- php/adm.php
+++ php/adm.php
@@ -5,21 +5,16 @@ $tgid = $_POST['tgid'];
 $username = $_POST['username'];
 $name = $_POST['name'];
 
-if ($data == "addadmin")
-{
+if ($data == "addadmin") {
 	$result = mysqli_query($link, "INSERT INTO `AdminsFeedback` (`tgid`, `status`) VALUES ('{$tgid}', 'admin')");
-}
-elseif ($data == "update")
-{
+} elseif ($data == "update") {
 	$result = mysqli_query($link, "UPDATE `AdminsFeedback` SET `name`='{$name}', `username`='{$username}' WHERE `tgid`='{$tgid}'");
-}
-elseif ($data == "delete")
-{
+} elseif ($data == "delete") {
 	$result = mysqli_query($link, "DELETE FROM `AdminsFeedback` WHERE `tgid`='{$tgid}'");
-}
-elseif ($data == "adduser")
-{
+} elseif ($data == "adduser") {
 	$result = mysqli_query($link, "INSERT INTO `AdminsFeedback` (`tgid`, `name`, `username`, `status`) VALUES ('{$tgid}','{$name}','{$username}', 'user') ");
+} elseif ($data == "find") {
+	$result = mysqli_query($link, "SELECT * FROM  AdminsFeedback WHERE tgid='{$tgid}'");
+	$row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	echo json_encode($row);
 }
-
-?>
