ca1f5508a08f9877953aebb6386b3d74390c6904
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2021-03-17 21:08:04 +0300

    Update AdminsFeedback.sql

--- AdminsFeedback.sql
+++ AdminsFeedback.sql
@@ -5,11 +5,14 @@ CREATE TABLE `AdminsFeedback` (
   `tgid` varchar(64) CHARACTER SET utf8 NOT NULL,
   `name` varchar(64) CHARACTER SET utf8 NOT NULL,
   `username` varchar(64) CHARACTER SET utf8 NOT NULL,
-  `status` varchar(64) CHARACTER SET utf8 NOT NULL
+  `status` varchar(64) CHARACTER SET utf8 NOT NULL,
+  `ban` varchar(3) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 INSERT INTO `AdminsFeedback` (`id`, `tgid`, `name`, `username`, `status`) VALUES
 (1, 'ТВОЙ АЙДИ В ТЕЛЕГЕ', 'ТВОЁ ИМЯ', 'ТВОЙ ЮЗЕРНЕЙМ', 'god');
+
 -- ВАЖНО! --> ПОСЛЕ ЮЗЕРНЕЙМА НЕ МЕНЯТЬ "god" НА ДРУГОЕ, ЭТО ЗНАЧИТ СТАТУС БОГА, А-ЛЯ САМЫЙ ГЛАВНЫЙ АДМИН !!!
+
 ALTER TABLE `AdminsFeedback`
   ADD PRIMARY KEY (`id`);
 ALTER TABLE `AdminsFeedback`
